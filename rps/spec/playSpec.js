8e9df98ff29914405c6116c6eb0a0138d0023872
Derek Lee <dereklee@downtothis.com>
2019-08-14 14:49:45 -0600

    Refactors RPS tests

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,128 +1,116 @@
 describe('play', () => {
-    let requests
+    let requests, observer
 
     beforeEach(() => {
         requests = new Game()
     })
 
-    it('rock vs scissors', () => {
-        let observer = jasmine.createSpyObj('observer', ['p1Wins'])
+    describe('player 1 wins scenarios', () => {
+        beforeEach(() => {
+            observer = jasmine.createSpyObj('observer', ['p1Wins'])
+        })
+
+        it('rock vs scissors', () => {
+            requests.play('rock', 'scissors', observer)
 
 
-        requests.play('rock', 'scissors', observer)
+            expect(observer.p1Wins).toHaveBeenCalled()
+        })
+
+        it('scissors vs paper', () => {
+            requests.play('scissors', 'paper', observer)
 
 
-        expect(observer.p1Wins).toHaveBeenCalled()
+            expect(observer.p1Wins).toHaveBeenCalled()
+        })
+
+        it('paper vs rock', () => {
+            requests.play('paper', 'rock', observer)
+
+
+            expect(observer.p1Wins).toHaveBeenCalled()
+        })
     })
 
-    it('scissors vs rock', () => {
-        let observer = jasmine.createSpyObj('observer', ['p2Wins'])
+    describe('player 2 wins scenarios', () => {
+        beforeEach(() => {
+            observer = jasmine.createSpyObj('observer', ['p2Wins'])
+        })
+
+        it('scissors vs rock', () => {
+            requests.play('scissors', 'rock', observer)
 
 
-        requests.play('scissors', 'rock', observer)
+            expect(observer.p2Wins).toHaveBeenCalled()
+        })
+
+        it('paper vs scissors', () => {
+            requests.play('paper', 'scissors', observer)
 
 
-        expect(observer.p2Wins).toHaveBeenCalled()
+            expect(observer.p2Wins).toHaveBeenCalled()
+        })
+
+        it('rock vs paper', () => {
+            requests.play('rock', 'paper', observer)
+
+
+            expect(observer.p2Wins).toHaveBeenCalled()
+        })
     })
 
-    it('scissors vs paper', () => {
-        let observer = jasmine.createSpyObj('observer', ['p1Wins'])
+    describe('tie scenarios', () => {
+        beforeEach(() => {
+            observer = jasmine.createSpyObj('observer', ['tie'])
+        })
+
+        it('rock vs rock', () => {
+            requests.play('rock', 'rock', observer)
 
 
-        requests.play('scissors', 'paper', observer)
+            expect(observer.tie).toHaveBeenCalled()
+        })
+
+        it('scissors vs scissors', () => {
+            requests.play('scissors', 'scissors', observer)
 
 
-        expect(observer.p1Wins).toHaveBeenCalled()
+            expect(observer.tie).toHaveBeenCalled()
+        })
+
+        it('paper vs paper', () => {
+            requests.play('paper', 'paper', observer)
+
+
+            expect(observer.tie).toHaveBeenCalled()
+        })
     })
 
-    it('paper vs scissors', () => {
-        let observer = jasmine.createSpyObj('observer', ['p2Wins'])
+    describe('invalid scenarios', () => {
+        beforeEach(() => {
+            observer = jasmine.createSpyObj('observer', ['invalid'])
+        })
+
+        it('invalid vs rock', () => {
+            requests.play('sailboat', 'rock', observer)
 
 
-        requests.play('paper', 'scissors', observer)
+            expect(observer.invalid).toHaveBeenCalled()
+        })
+
+        it('rock vs invalid', () => {
+            requests.play('rock', 'sailboat', observer)
 
 
-        expect(observer.p2Wins).toHaveBeenCalled()
-    })
+            expect(observer.invalid).toHaveBeenCalled()
+        })
 
-    it('paper vs rock', () => {
-        let observer = jasmine.createSpyObj('observer', ['p1Wins'])
-
-
-        requests.play('paper', 'rock', observer)
+        it('invalid vs invalid', () => {
+            requests.play('sailboat', 'sailboat', observer)
 
 
-        expect(observer.p1Wins).toHaveBeenCalled()
-    })
-
-    it('rock vs paper', () => {
-        let observer = jasmine.createSpyObj('observer', ['p2Wins'])
-
-
-        requests.play('rock', 'paper', observer)
-
-
-        expect(observer.p2Wins).toHaveBeenCalled()
-    })
-
-    it('rock vs rock', () => {
-        let observer = jasmine.createSpyObj('observer', ['tie'])
-
-
-        requests.play('rock', 'rock', observer)
-
-
-        expect(observer.tie).toHaveBeenCalled()
-    })
-
-    it('scissors vs scissors', () => {
-        let observer = jasmine.createSpyObj('observer', ['tie'])
-
-
-        requests.play('scissors', 'scissors', observer)
-
-
-        expect(observer.tie).toHaveBeenCalled()
-    })
-
-    it('paper vs paper', () => {
-        let observer = jasmine.createSpyObj('observer', ['tie'])
-
-
-        requests.play('paper', 'paper', observer)
-
-
-        expect(observer.tie).toHaveBeenCalled()
-    })
-
-    it('invalid vs rock', () => {
-        let observer = jasmine.createSpyObj('observer', ['invalid'])
-
-
-        requests.play('sailboat', 'rock', observer)
-
-
-        expect(observer.invalid).toHaveBeenCalled()
-    })
-
-    it('rock vs invalid', () => {
-        let observer = jasmine.createSpyObj('observer', ['invalid'])
-
-
-        requests.play('rock', 'sailboat', observer)
-
-
-        expect(observer.invalid).toHaveBeenCalled()
-    })
-
-    it('invalid vs invalid', () => {
-        let observer = jasmine.createSpyObj('observer', ['invalid'])
-
-
-        requests.play('sailboat', 'sailboat', observer)
-
-
-        expect(observer.invalid).toHaveBeenCalled()
+            expect(observer.invalid).toHaveBeenCalled()
+        })
     })
 })
 
